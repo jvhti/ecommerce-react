f56ff8b60b1c1467df3d2698284fb8275c55c7b9
João Víctor <jvhti@hotmail.com>
2019-05-30 21:02:22 -0300

    Improving 'Login Modal'

--- src/Components/LoginModal/LoginModal.js
+++ src/Components/LoginModal/LoginModal.js
@@ -5,6 +5,32 @@ class LoginModal extends React.Component{
 
   constructor(props){
     super(props);
+
+    this.labelEmail = React.createRef();
+    this.inputEmail = React.createRef();
+    this.labelPassword = React.createRef();
+  }
+
+  componentDidMount(){
+    this.inputEmail.current.focus();
+  }
+
+  updateFieldValidation(ev){
+    const tgLabel = (ev.target.type === "email") ? this.labelEmail.current : this.labelPassword.current;
+
+    if(ev.target.value === ""){
+      tgLabel.classList.remove("valid");
+      tgLabel.classList.remove("invalid");
+      return;
+    }
+
+    if(ev.target.validity.valid){
+      tgLabel.classList.add("valid");
+      tgLabel.classList.remove("invalid");
+    }else{
+      tgLabel.classList.add("invalid");
+      tgLabel.classList.remove("valid");
+    }
   }
   
   render() {
@@ -13,17 +39,17 @@ class LoginModal extends React.Component{
           <h5 className="login_modal__title">Login</h5>
           <div className="login_modal__form">
             <div className="login_modal__form__field">
-              <input placeholder="example@example.com" type="email"/>
-              <label tabindex="-1">E-mail: </label>
+              <label ref={this.labelEmail}>E-mail: </label>
+              <input ref={this.inputEmail} placeholder="example@example.com" type="email" required onChange={this.updateFieldValidation.bind(this)}/>
             </div>
             <div className="login_modal__form__field">
-              <a tabindex="0" href="#">Forgot your password?</a>
-              <input placeholder="Your Password" type="password" required/>
-              <label tabindex="-1">Password: </label>
+              <label ref={this.labelPassword}>Password: </label>
+              <input placeholder="Your Password" type="password" required onChange={this.updateFieldValidation.bind(this)}/>
+              <a href="#">Forgot your password?</a>
             </div>
             <div className="login_modal__form__actions">
-              <button className="btn btn--default btn--border">Register</button>
               <button className="btn btn--primary">Login</button>
+              <button className="btn btn--default btn--border">Register</button>
             </div>
           </div>
         </div>
